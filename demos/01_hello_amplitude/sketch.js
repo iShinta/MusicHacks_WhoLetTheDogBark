3ae8240bbbfa879c726b0312cfeb511f0e42c016
iShinta <ho_minh_tri@yahoo.fr>
2017-04-15 14:48:50 -0500

    Add Amplitude Threshol

--- demos/01_hello_amplitude/sketch.js
+++ demos/01_hello_amplitude/sketch.js
@@ -3,9 +3,15 @@
  * and map it to the ellipse position.
  */
 
+var input;
+
 var mic, soundFile;
 var amplitude;
 var mapMax = 1.0;
+
+var threshold = 0.1;
+var cutoff = 0;
+var decayRate = 0.95;
 
 function preload() {
   // load the sound, but don't play it yet
@@ -14,28 +20,76 @@ function preload() {
 
 function setup() {
   c = createCanvas(windowWidth, windowHeight);
-  background(0);
-  fill(255);
-  noStroke();
+  background(255);
+  // background(0);
+  // fill(255);
+  // noStroke();
 
   mic = new p5.AudioIn();
   mic.start();
 
+  input = mic;
+
   // soundFile.play();
 
-  amplitude = new p5.Amplitude();
-  amplitude.setInput(mic);
+  // amplitude = new p5.Amplitude();
+  // amplitude.setInput(soundFile);
+  // amplitude.setInput(mic);
   // amplitude.smooth(0.8); // <-- try this!
 }
 
 function draw() {
-  background(0);
+  // Get the overall volume (between 0 and 1.0)
+  var volume = input.getLevel();
 
-  var level = amplitude.getLevel();
-  text('Amplitude: ' + level, 20, 20);
-  text('mapMax: ' + mapMax, 20, 40);
+  // If the volume > threshold + cutoff, a rect is drawn at a random location.
+  // The louder the volume, the larger the rectangle.
+  if (volume > threshold + cutoff) {
+    stroke(0);
+    fill(0, 100);
+    //rect(random(40, width), random(height), volume*50, volume*50);
+    drawPaw(random(40, width), random(height), volume*100, volume*100); //x, y, width, height
 
-  // map ellipse height
-  var ellipseHeight = map(level, 0, mapMax, height, 0);
-  ellipse(width/2, ellipseHeight, 100, 100);
+    // increase the cutoff
+    cutoff = 0.5;
+  }
+
+  //
+  cutoff = cutoff * decayRate;
+
+  // Graph the overall potential volume, w/ a line at the threshold
+  var y = map(volume, 0, 1, height, 0);
+  var ythreshold = map(threshold + cutoff, 0, 1, height, 0);
+
+  noStroke();
+  fill(175);
+  rect(0, 0, 20, height);
+
+  // Then draw a rectangle on the graph, sized according to volume
+  fill(0);
+  rect(0, y, 20, y);
+  stroke(0);
+  line(0, ythreshold, 19, ythreshold);
+}
+
+function drawPaw(x, y, w, h){
+  ellipseMode(CORNER);
+  fill(255); //White
+  ellipse(x+w*0.1, y, w*0.8, h*0.8);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x, y, w/7*1.5, h/2);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+2*w/7, y-h/3, w/7*1.5, h/1.8);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+4*w/7, y-h/3, w/7*1.5, h/1.8);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+6*w/7, y, w/7*1.5, h/2);
 }
--- demos/01c_amplitude_threshold/sketch.js
+++ demos/01c_amplitude_threshold/sketch.js
@@ -8,7 +8,7 @@
  *
  *  The cutoff decays every frame by a decay rate, so a beat can
  *  be triggered again.
- *  
+ *
  */
 
 var input;
@@ -18,7 +18,7 @@ var cutoff = 0;
 var decayRate = 0.95;
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(1280, 720);
   background(255);
 
   // Create an Audio input
@@ -30,12 +30,13 @@ function draw() {
   // Get the overall volume (between 0 and 1.0)
   var volume = input.getLevel();
 
-  // If the volume > threshold + cutoff, a rect is drawn at a random location. 
+  // If the volume > threshold + cutoff, a rect is drawn at a random location.
   // The louder the volume, the larger the rectangle.
   if (volume > threshold + cutoff) {
     stroke(0);
     fill(0, 100);
-    rect(random(40, width), random(height), volume*50, volume*50);
+    //rect(random(40, width), random(height), volume*50, volume*50);
+    drawPaw(random(40, width), random(height), volume*100, volume*100); //x, y, width, height
 
     // increase the cutoff
     cutoff = 0.5;
@@ -57,4 +58,26 @@ function draw() {
   rect(0, y, 20, y);
   stroke(0);
   line(0, ythreshold, 19, ythreshold);
+}
+
+function drawPaw(x, y, w, h){
+  ellipseMode(CORNER);
+  fill(255); //White
+  ellipse(x, y, w, h);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x, y-h/4, w/7*1.5, h/1.4);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+2*w/7, y-h/2, w/7*1.5, h/1.4);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+4*w/7, y-h/2, w/7*1.5, h/1.4);
+
+  ellipseMode(CORNER);
+  fill(100); //Gray
+  ellipse(x+6*w/7, y-h/4, w/7*1.5, h/1.4);
 }
