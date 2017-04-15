46f8639a08358b3ca61ce3a7b1eaf8b885bc34d9
iShinta <ho_minh_tri@yahoo.fr>
2017-04-15 16:10:25 -0500

    Background Color

--- demos/01_hello_amplitude/sketch.js
+++ demos/01_hello_amplitude/sketch.js
@@ -9,9 +9,15 @@ var mic, soundFile;
 var amplitude;
 var mapMax = 1.0;
 
+var backgroundColor;
+
 var threshold = 0.1;
 var cutoff = 0;
 var decayRate = 0.95;
+
+var paws = new Array(50);
+var pawsx = new Array(50);
+var pawsy = new Array(50);
 
 function preload() {
   // load the sound, but don't play it yet
@@ -24,6 +30,8 @@ function setup() {
   // background(0);
   // fill(255);
   // noStroke();
+
+  backgroundColor = color( random(0,255), random(0,255), random(0,255) );
 
   mic = new p5.AudioIn();
   mic.start();
@@ -39,19 +47,32 @@ function setup() {
 }
 
 function draw() {
+  background(backgroundColor);
+
   // Get the overall volume (between 0 and 1.0)
   var volume = input.getLevel();
 
   // If the volume > threshold + cutoff, a rect is drawn at a random location.
   // The louder the volume, the larger the rectangle.
   if (volume > threshold + cutoff) {
+    backgroundColor = color( random(0,255), random(0,255), random(0,255) );
+
+    paws.push(volume);
+    paws.splice(0, 1);
+
+    pawsx.push(random(40, width));
+    pawsx.splice(0, 1);
+
+    pawsy.push(random(height));
+    pawsy.splice(0, 1);
+
+    cutoff = 0.5;
+  }
+
+  for(var i = 0; i < paws.length; i++){
     stroke(0);
     fill(0, 100);
-    //rect(random(40, width), random(height), volume*50, volume*50);
-    drawPaw(random(40, width), random(height), volume*100, volume*100); //x, y, width, height
-
-    // increase the cutoff
-    cutoff = 0.5;
+    drawPaw(pawsx[i], pawsy[i], paws[i]*100, paws[i]*100); //x, y, width, height
   }
 
   //
@@ -75,21 +96,21 @@ function draw() {
 function drawPaw(x, y, w, h){
   ellipseMode(CORNER);
   fill(255); //White
-  ellipse(x+w*0.1, y, w*0.8, h*0.8);
+  ellipse(x+w*0.15, y-h/8, w*0.8, h*0.8);
 
   ellipseMode(CORNER);
   fill(100); //Gray
-  ellipse(x, y, w/7*1.5, h/2);
+  ellipse(x, y, w*0.3, h*0.3);
 
   ellipseMode(CORNER);
   fill(100); //Gray
-  ellipse(x+2*w/7, y-h/3, w/7*1.5, h/1.8);
+  ellipse(x+2*w/7, y-h/3, w*0.3, h*0.3);
 
   ellipseMode(CORNER);
   fill(100); //Gray
-  ellipse(x+4*w/7, y-h/3, w/7*1.5, h/1.8);
+  ellipse(x+4*w/7, y-h/3, w*0.3, h*0.3);
 
   ellipseMode(CORNER);
   fill(100); //Gray
-  ellipse(x+6*w/7, y, w/7*1.5, h/2);
+  ellipse(x+6*w/7, y, w*0.3, h*0.3);
 }
